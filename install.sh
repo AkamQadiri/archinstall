e06869517e27a0cbeca9de9cfb609a8c6e1759ca
AkamQadiri <akamq@hotmail.com>
2022-10-28 02:12:14 +0200

    Updated driver packages

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ export ROOT_PARTITION="$DEVICE"p2
 
 #Packages
 export X_PACKAGES="xorg xorg-xinit xorg-apps xdg-utils picom"
-export DRIVER_PACKAGES="intel-ucode libva-intel-driver vulkan-intel xf86-video-nouveau libva-mesa-driver mesa mesa-utils"
+export DRIVER_PACKAGES="intel-ucode mesa mesa-utils libva-mesa-driver libva-intel-driver vulkan-icd-loader vulkan-intel nvidia nvidia-utils"
 export AUDIO_PACKAGES="pipewire pipewire-alsa pipewire-pulse pipewire-jack"
 export FONT_PACKAGES="ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-font-awesome"
 export ADDITIONAL_PACKAGES="git vim htop firefox feh python-pywal playerctl"
