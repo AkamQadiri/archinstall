8f656b0511f5b680a9e2e533ede7b31480c09d55
AkamQadiri <akamq@hotmail.com>
2022-12-27 23:16:44 +0100

    Added xdg-user-dirs, pavucontrol, czkawka-gui-bin

--- install.sh
+++ install.sh
@@ -21,14 +21,14 @@ export EFI_PARTITION="$DEVICE"p1
 export ROOT_PARTITION="$DEVICE"p2
 
 #Packages
-export X_PACKAGES="xorg xorg-xinit xorg-apps xdg-utils numlockx xdotool perl-file-mimeinfo picom lxsession-gtk3 unclutter"
+export X_PACKAGES="xorg xorg-xinit xorg-apps xdg-utils xdg-user-dirs numlockx xdotool perl-file-mimeinfo picom lxsession-gtk3 unclutter"
 export DRIVER_PACKAGES="intel-ucode mesa mesa-utils libva-mesa-driver libva-vdpau-driver libva-intel-driver vulkan-icd-loader vulkan-intel nvidia nvidia-utils"
-export AUDIO_PACKAGES="pipewire pipewire-alsa pipewire-pulse pipewire-jack noise-suppression-for-voice"
+export AUDIO_PACKAGES="pipewire pipewire-alsa pipewire-pulse pipewire-jack noise-suppression-for-voice pavucontrol"
 export FONT_PACKAGES="ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-font-awesome"
 export ADDITIONAL_PACKAGES="zip unzip git vim htop firefox feh python-pywal playerctl"
 
 #YAY Packages will only be installed if git is present
-export YAY_PACKAGES="pfetch nnn-icons" 
+export YAY_PACKAGES="pfetch nnn-icons czkawka-gui-bin" 
 
 #Systemctl services
 export SYSTEMCTL_GLOBAL_SERVICES="pipewire pipewire-media-session pipewire-pulse"
