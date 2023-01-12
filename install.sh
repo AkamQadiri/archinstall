7ff12daae9d8a6b30d74f41f8b75793c34002b18
AkamQadiri <akamq@hotmail.com>
2023-01-12 07:49:31 +0100

    Added fonts

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ export ROOT_PARTITION="$DEVICE"p2
 export X_PACKAGES="xorg xorg-xinit xorg-apps xdg-utils numlockx xdotool perl-file-mimeinfo picom lxsession-gtk3 unclutter"
 export DRIVER_PACKAGES="intel-ucode mesa mesa-utils libva-mesa-driver libva-vdpau-driver libva-intel-driver vulkan-icd-loader vulkan-intel nvidia nvidia-utils"
 export AUDIO_PACKAGES="pipewire pipewire-alsa pipewire-pulse pipewire-jack noise-suppression-for-voice pavucontrol"
-export FONT_PACKAGES="ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-font-awesome"
+export FONT_PACKAGES="noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-dejavu ttf-liberation ttf-hack ttf-font-awesome"
 export ADDITIONAL_PACKAGES="zip unzip git vim htop firefox feh python-pywal playerctl"
 #Uncomment the line below to install and configure libvirt (Adds the user to libvirt group automatically)
 #export LIBVIRT_PACKAGES="qemu-desktop qemu-emulators-full dnsmasq dmidecode bridge-utils openbsd-netcat virt-manager"
